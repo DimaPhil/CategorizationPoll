6a6b20260a85a53f35f24be874cea5ae88c13702
DimaPhil <dima239philippov@gmail.com>
2016-07-12 19:27:51 +0300

    added jar of full project

--- create_jar.sh
+++ create_jar.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+cd src
+rm -rf META-INF
+mkdir META-INF
+echo Manifest-Version: 1.0> META-INF/manifest.1
+echo Main-Class: com.codefights.poll.Main> META-INF/manifest.1
+javac -cp .:../lib/markdown4j-2.2.jar:pegdown-1.6.0.jar com/codefights/poll/Main.java
+jar -cfm Poll.jar META-INF/manifest.1 com/codefights/poll/*.class
